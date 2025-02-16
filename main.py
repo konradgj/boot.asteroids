d5d12c415f5c07acd20bc074e3e2ea83998d9e2b
konradgj <konrad.gjerstad@gmail.com>
2025-02-16 14:42:43 +0100

    L3: fps

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    gClock = pygame.time.Clock()
+    dt = 0
 
     while True:
         screen.fill(pygame.Color(0, 0, 0))
@@ -17,6 +19,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        dt = gClock.tick(60) / 1000
 
 
 if __name__ == "__main__":
